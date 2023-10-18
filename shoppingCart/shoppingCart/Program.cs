a78ecb273aa6db545ce35bf3c3ac013be65094c1
prem <premt8566@gmail.com>
2023-10-18 16:24:35 +0530

    Test23

--- shoppingCart/shoppingCart/Program.cs
+++ shoppingCart/shoppingCart/Program.cs
@@ -1,8 +1,12 @@
+using shoppingCart.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
 builder.Services.AddControllers();
+
+
 
 var app = builder.Build();
 
