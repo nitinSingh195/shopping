a6e0c57c045f5e90dbbee40b773b81308ff57852
prem <premt8566@gmail.com>
2023-10-18 14:56:23 +0530

    test

--- shoppingCart/shoppingCart/Model/Users.cs
+++ shoppingCart/shoppingCart/Model/Users.cs
@@ -9,6 +9,6 @@
         public string Email { get; set; }
         public DateTime RegistrationDate { get; set; }
         public DateTime? LastLoginDate { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive1 { get; set; }
     }
 }
