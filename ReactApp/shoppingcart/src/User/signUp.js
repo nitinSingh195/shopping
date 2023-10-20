474aa3e3468bfade99469edbd6961a245d9f03d8
Nitin Singh <Singh-Nitin@aramark.com>
2023-10-20 11:07:41 +0530

    SignUp form in FE

--- ReactApp/shoppingcart/src/User/signUp.js
+++ ReactApp/shoppingcart/src/User/signUp.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Signup() {
+    const [formData, setFormData] = useState({
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const signupUser = () => {
+        axios.post('https://api.example.com/signup', formData)
+            .then(response => {
+                console.log('Signup successful:', response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
+
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
@@ -9,26 +33,68 @@ function Signup() {
                     <form>
                         <div className="mb-3">
                             <label htmlFor="username" className="form-label">Username</label>
-                            <input type="text" className="form-control" id="username" placeholder="Username" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="username"
+                                name="username"
+                                placeholder="Username"
+                                value={formData.username}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="firstName" className="form-label">FirstName</label>
-                            <input type="firstName" className="form-control" id="firstName" placeholder="First Name" />
+                            <label htmlFor="firstName" className="form-label">First Name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="firstName"
+                                name="firstName"
+                                placeholder="First Name"
+                                value={formData.firstName}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="lastName" className="form-label">LastName</label>
-                            <input type="lastName" className="form-control" id="lastName" placeholder="Last Name" />
+                            <label htmlFor="lastName" className="form-label">Last Name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="lastName"
+                                name="lastName"
+                                placeholder="Last Name"
+                                value={formData.lastName}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="email" placeholder="Email" />
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="password" placeholder="Password" />
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="mb-3">
-                            <button type="submit" className="btn btn-primary">Sign Up</button>
+                            <button type="button" className="btn btn-primary" onClick={signupUser}>
+                                Sign Up
+                            </button>
                         </div>
                     </form>
                 </div>
