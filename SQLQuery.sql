9aedb5815794f70ee5508eec392c564794c87c6f
prem <premt8566@gmail.com>
2023-10-20 11:30:00 +0530

    Sql query

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,14 +1,35 @@
-CREATE TABLE Users (
-    UserID INT PRIMARY KEY IDENTITY(1,1),
-    Username NVARCHAR(50) NOT NULL,
-    FirstName NVARCHAR(50),
-    LastName NVARCHAR(50),
-    Email NVARCHAR(100) NOT NULL,
-    PasswordHash NVARCHAR(100) NULL,
-    RegistrationDate DATETIME NULL,
-    LastLoginDate DATETIME,
-    IsActive BIT NOT NULL default 1 
-);
+USE [ShoppingCart]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 20-10-2023 11.29.17 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](50) NOT NULL,
+	[FirstName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[Email] [nvarchar](100) NOT NULL,
+	[PasswordHash] [nvarchar](100) NULL,
+	[RegistrationDate] [datetime] NULL,
+	[LastLoginDate] [datetime] NULL,
+	[IsActive] [bit] NOT NULL,
+	[Role] [varchar](256) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Users] ADD  DEFAULT ((1)) FOR [IsActive]
+GO
+
+
 go
 USE [ShoppingCart]
 GO
