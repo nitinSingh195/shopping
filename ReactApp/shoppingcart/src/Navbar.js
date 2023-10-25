46867dad101265f747e83268a58987340de0fc7e
Nitin Singh <Singh-Nitin@aramark.com>
2023-10-25 11:49:31 +0530

    added Badges

--- ReactApp/shoppingcart/src/Navbar.js
+++ ReactApp/shoppingcart/src/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Navbar = () => {
+    const userName = 'John Doe'; // Replace with the actual user name
+    const cartItemCount = 5; // Replace with the actual cart item count
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">Shopping Cart</a>
@@ -27,6 +30,18 @@ const Navbar = () => {
                         <a className="nav-link" href="/contact">Profile</a>
                     </li>
                 </ul>
+            </div>
+            <div className="ml-auto">
+                {/* <span className="navbar-text">
+                    <span className="badge bg-primary mr-2">{cartItemCount}</span>
+                    Welcome, {userName}
+                </span> */}
+                <button type="button" class="btn btn-primary position-relative">
+                    My Cart
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    {cartItemCount}
+                    </span>
+                </button>
             </div>
         </nav>
     );
